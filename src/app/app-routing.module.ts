d018e1e05d775515f0974b33d38deedc4c8e8208
Shade <form3pelican@gmail.com>
2022-06-19 02:33:01 +0400

    vdv

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'admin', loadChildren: () => import('./features/admin/admin.module').then(m => m.AdminModule) }];
+const routes: Routes = [{ path: 'admin', loadChildren: () => import('./features/admin/admin.module').then(m => m.AdminModule) },
+{
+  path: '',
+  redirectTo: 'admin',
+  pathMatch: 'full'
+},];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
