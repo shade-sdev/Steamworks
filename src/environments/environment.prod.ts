540c443dd8a2bb2a18dd2eba8d9a9bfe3755c5e9
Shade <form3pelican@gmail.com>
2022-07-12 16:53:15 +0400

    change env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   baseUrl: 'https://steamworks.herokuapp.com',
-  discordTokenUrl: 'https://discord.com/api/oauth2/authorize?client_id=988449942702014484&redirect_uri=https%3A%2F%2Fsteamworks.tk&response_type=code&scope=identify%20email%20connections%20guilds%20guilds.join%20guilds.members.read%20gdm.join'
+  discordTokenUrl: 'https://discord.com/api/oauth2/authorize?client_id=988449942702014484&redirect_uri=https%3A%2F%2Fsteamworks.tk&response_type=code&scope=identify%20guilds%20guilds.join'
 };
